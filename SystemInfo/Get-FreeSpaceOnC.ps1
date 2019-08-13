b4fe0572356ea3da4b17b80e26b248ca2cd892da
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2019-08-13 14:42:07 +0200

    Monitor info

---
+++ SystemInfo/Get-FreeSpaceOnC.ps1
@@ -0,0 +1,9 @@
+﻿#
+# Provjera koliko je slobodnog prostora na C disku kod navedenih računara
+#
+
+"mvidovic", "aprodanovic", "dbojic", "publicserver","server2" | foreach {
+Get-WmiObject -Class Win32_LogicalDisk `
+-ComputerName $_ -Filter "DeviceId='C:'" } |
+Format-Table SystemName, @{Name="Free"; 
+Expression={[math]::round($($_.FreeSpace/1GB), 2)}} -auto
